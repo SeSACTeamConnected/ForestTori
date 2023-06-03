5d0429d370406382042288fa46c733ad0799a473
kdb1217 <mokdb17@gmail.com>
2023-06-03 11:14:34 +0900

    initial setting

---
+++ Tori's Forest/Tori's Forest/Tori_s_ForestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tori_s_ForestApp.swift
+//  Tori's Forest
+//
+//  Created by 김다빈 on 2023/06/03.
+//
+
+import SwiftUI
+
+@main
+struct Tori_s_ForestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
